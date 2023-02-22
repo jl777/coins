93f52024bea9682d562055c38cf8c851a4bea2d9
gcharang <21151592+gcharang@users.noreply.github.com>
2023-02-22 14:54:42 +0530

    add image resize to workflow and update readme info about icons

--- utils/icons_resize.sh
+++ utils/icons_resize.sh
@@ -3,7 +3,9 @@
 scriptpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd $scriptpath
-cd ../icons
+cd ..
+rsync -a -v --ignore-existing icons_original/ icons/ #copy missing images from icons_original to icons
+cd icons
 
 IMAGES_TO_RESIZE="$(find . -iname "*.png" -type f -exec identify -format '%w %h %i\n' '{}' \; | awk '$1>128 || $2>128' | awk '{print $3}')"
 
