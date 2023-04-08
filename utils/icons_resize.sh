f0d50f83c8f4df4ee1e9c521c54dc271daebf509
cipig <cipianon@gmail.com>
2023-04-08 19:47:21 +0000

    fix

--- utils/icons_resize.sh
+++ utils/icons_resize.sh
@@ -10,5 +10,5 @@ cd icons
 IMAGES_TO_RESIZE="$(find . -iname "*.png" -type f -exec identify -format '%w %h %i\n' '{}' \; | awk '$1>120 || $2>120' | awk '{print $3}')"
 
 echo $IMAGES_TO_RESIZE | while read line; do
-    mogrify -trim +repage -fuzz 20% -resize 128x128  $line # needs imagemagick installed
+    mogrify -trim +repage -fuzz 20% -resize 128x128 $line # needs imagemagick installed
 done
