9b7da829ad6b50801dd0bb107e41f949d78cbb4d
cipig <cipianon@gmail.com>
2023-04-09 01:22:24 +0000

    120x120

--- utils/icons_resize.sh
+++ utils/icons_resize.sh
@@ -7,8 +7,8 @@ cd ..
 rsync -a -v --ignore-existing icons_original/ icons/ #copy missing images from icons_original to icons
 cd icons
 
-IMAGES_TO_RESIZE="$(find . -iname "*.png" -type f -exec identify -format '%w %h %i\n' '{}' \; | awk '$1>126 || $2>126' | awk '{print $3}')"
+IMAGES_TO_RESIZE="$(find . -iname "*.png" -type f -exec identify -format '%w %h %i\n' '{}' \; | awk '$1>120 || $2>120' | awk '{print $3}')"
 
 echo $IMAGES_TO_RESIZE | while read line; do
-    mogrify -trim +repage -fuzz 20% -resize 126x126 $line # needs imagemagick installed
+    mogrify -trim +repage -fuzz 20% -resize 120x120 $line # needs imagemagick installed
 done
